f6e2790b54e88da96b52b273dcb3ad2a3a30fbc6
gudzan <andreeva.anva@gmail.com>
2023-09-29 22:08:40 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -164,3 +164,15 @@ new Swiper(".experts__body", {
         }
     },
 });
+
+const input = document.querySelector("#phone");
+window.intlTelInput(input, {
+    initialCountry: "auto",
+    geoIpLookup: callback => {
+        fetch("https://ipapi.co/json")
+            .then(res => res.json())
+            .then(data => callback(data.country_code))
+            .catch(() => callback("ru"));
+    },
+    utilsScript: "/intl-tel-input/js/utils.js?1695806485509" // just for formatting/placeholders etc
+});
