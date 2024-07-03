40fee7109574a03a75bf32417e702c36ece1b4c2
gudzan <andreeva.anva@gmail.com>
2024-07-03 12:11:41 +0300

    doc

--- js/main.js
+++ js/main.js
@@ -1,121 +1,127 @@
-const burger = document.querySelector('.burger');
-const headerMenu = document.querySelector('.header__menu-list');
-const headerItems = document.querySelectorAll('.header__menu-item');
-const overlay = document.querySelector('.overlay');
-const autorisationOpen = document.querySelector('.header__top-button');
-const autorisationClose = document.querySelector('.autorisation__close');
-const autorisationBlock = document.querySelector('.autorisation');
-const marketsCountries = document.querySelectorAll('.markets__country');
-const marketsCountriesBox = document.querySelectorAll('.markets__map-box');
+const burger = document.querySelector(".burger");
+const headerMenu = document.querySelector(".header__menu-list");
+const headerItems = document.querySelectorAll(".header__menu-item");
+const overlay = document.querySelector(".overlay");
+const autorisationOpen = document.querySelector(".header__top-button");
+const autorisationClose = document.querySelector(".autorisation__close");
+const autorisationBlock = document.querySelector(".autorisation");
+const marketsCountries = document.querySelectorAll(".markets__country");
+const marketsCountriesBox = document.querySelectorAll(".markets__map-box");
 
-burger.addEventListener('click', toggleBurger);
-headerItems.forEach(item => {
-    item.addEventListener('click', toggleBurger);
+burger.addEventListener("click", toggleBurger);
+headerItems.forEach((item) => {
+    item.addEventListener("click", toggleBurger);
 });
-overlay.addEventListener('click', closeAll);
-autorisationOpen.addEventListener('click', openAutorisation);
-autorisationClose.addEventListener('click', closeAutorisation);
-marketsCountries.forEach(item => {
-    item.addEventListener('click', toggleMarketsMap);
+overlay.addEventListener("click", closeAll);
+autorisationOpen.addEventListener("click", openAutorisation);
+autorisationClose.addEventListener("click", closeAutorisation);
+marketsCountries.forEach((item) => {
+    item.addEventListener("click", toggleMarketsMap);
 });
-
 
 function openAutorisation() {
-    autorisationBlock.classList.add('autorisation--open');
-    overlay.classList.add('overlay--open');
-    document.body.classList.add('body-not-scroll');
-};
+    autorisationBlock.classList.add("autorisation--open");
+    overlay.classList.add("overlay--open");
+    document.body.classList.add("body-not-scroll");
+}
 
 function closeAutorisation() {
-    autorisationBlock.classList.remove('autorisation--open');
-    overlay.classList.remove('overlay--open');
-    document.body.classList.remove('body-not-scroll');
-};
+    autorisationBlock.classList.remove("autorisation--open");
+    overlay.classList.remove("overlay--open");
+    document.body.classList.remove("body-not-scroll");
+}
 
 function closeAll() {
-    if (overlay.classList.contains('overlay--open')) {
+    if (overlay.classList.contains("overlay--open")) {
         closeBurger();
         closeAutorisation();
     }
-};
+}
 
 function closeBurger() {
-    burger.classList.remove('burger--open');
-    headerMenu.classList.remove('header__menu-list--open');
-    overlay.classList.remove('overlay--open');
-    document.body.classList.remove('body-not-scroll');
-};
+    burger.classList.remove("burger--open");
+    headerMenu.classList.remove("header__menu-list--open");
+    overlay.classList.remove("overlay--open");
+    document.body.classList.remove("body-not-scroll");
+}
 
 function toggleBurger() {
     if (visualViewport.width < 760) {
-        burger.classList.toggle('burger--open');
-        headerMenu.classList.toggle('header__menu-list--open');
-        overlay.classList.toggle('overlay--open');
-        document.body.classList.toggle('body-not-scroll');
+        burger.classList.toggle("burger--open");
+        headerMenu.classList.toggle("header__menu-list--open");
+        overlay.classList.toggle("overlay--open");
+        document.body.classList.toggle("body-not-scroll");
     }
-};
+}
 
 function playVideo() {
-    document.querySelector('.video__box-black').classList.remove('video__box-black--visibile');
-    document.querySelector('.video__box').classList.add('video__box--visible');
-    var iframe = document.querySelector('iframe');
+    document
+        .querySelector(".video__box-black")
+        .classList.remove("video__box-black--visibile");
+    document.querySelector(".video__box").classList.add("video__box--visible");
+    var iframe = document.querySelector("iframe");
     var player = new Vimeo.Player(iframe);
     player.play();
-};
+}
 
 function toggleArrow(element) {
-    element.classList.toggle('program__accordion-title-arrow--active');
+    element.classList.toggle("program__accordion-title-arrow--active");
+}
 
-};
-
-
-window.addEventListener('resize', function () {
-    if (visualViewport.width > 760 && !autorisationBlock.classList.contains('autorisation--open')) {
+window.addEventListener("resize", function () {
+    if (
+        visualViewport.width > 760 &&
+        !autorisationBlock.classList.contains("autorisation--open")
+    ) {
         closeBurger();
     }
 });
 
 function toggleMarketsMap() {
-    marketsCountries.forEach(item => {
-        item.classList.remove('markets__country--active');
-    })
-    this.classList.add('markets__country--active');
-    document.querySelector('.markets__map-box--active').classList.toggle('markets__map-box--active');
+    marketsCountries.forEach((item) => {
+        item.classList.remove("markets__country--active");
+    });
+    this.classList.add("markets__country--active");
+    document
+        .querySelector(".markets__map-box--active")
+        .classList.toggle("markets__map-box--active");
 
-    if (this.classList.contains('markets__country-east')) {
-        document.querySelector('.markets__map-box--east').classList.add('markets__map-box--active');
+    if (this.classList.contains("markets__country-east")) {
+        document
+            .querySelector(".markets__map-box--east")
+            .classList.add("markets__map-box--active");
     }
-    if (this.classList.contains('markets__country-asia')) {
-        document.querySelector('.markets__map-box--asia').classList.add('markets__map-box--active');
+    if (this.classList.contains("markets__country-asia")) {
+        document
+            .querySelector(".markets__map-box--asia")
+            .classList.add("markets__map-box--active");
     }
-    if (this.classList.contains('markets__country-america')) {
-        document.querySelector('.markets__map-box--america').classList.add('markets__map-box--active');
+    if (this.classList.contains("markets__country-america")) {
+        document
+            .querySelector(".markets__map-box--america")
+            .classList.add("markets__map-box--active");
     }
-    if (this.classList.contains('markets__country-africa')) {
-        document.querySelector('.markets__map-box--africa').classList.add('markets__map-box--active');
+    if (this.classList.contains("markets__country-africa")) {
+        document
+            .querySelector(".markets__map-box--africa")
+            .classList.add("markets__map-box--active");
     }
-};
+}
 
-const accordionTitle = document.querySelectorAll('.program__accordion-title');
-accordionTitle.forEach(accordionTitleItem => {
-    accordionTitleItem.addEventListener('click', function (e) {
+const accordionTitle = document.querySelectorAll(".program__accordion-title");
+accordionTitle.forEach((accordionTitleItem) => {
+    accordionTitleItem.addEventListener("click", function (e) {
         e.preventDefault();
         let content = accordionTitleItem.nextElementSibling;
-        if (content.classList.contains('program__accordion-content--active')) {
+        if (content.classList.contains("program__accordion-content--active")) {
             content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = "360px";
         }
-        else {
-            content.style.maxHeight = '360px';
-        }
-        content.classList.toggle('program__accordion-content--active');
+        content.classList.toggle("program__accordion-content--active");
         let arrow = accordionTitleItem.lastElementChild;
-        arrow.classList.toggle('program__accordion-title-arrow--active');
-    })
-});
-
-window.addEventListener('scroll', function () {
-    /*const scrollPosition = window.scrollY;
-    console.log(scrollPosition);*/
+        arrow.classList.toggle("program__accordion-title-arrow--active");
+    });
 });
 
 new Swiper(".gallery__box", {
@@ -136,7 +142,7 @@ new Swiper(".gallery__box", {
         },
         1000: {
             slidesPerView: 5,
-        }
+        },
     },
 });
 
@@ -146,7 +152,7 @@ new Swiper(".experts__body", {
     watchSlidesProgress: false,
     navigation: {
         nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
+        prevEl: ".swiper-button-prev",
     },
     freeMode: true,
     breakpoints: {
@@ -161,18 +167,18 @@ new Swiper(".experts__body", {
         },
         1200: {
             slidesPerView: 4,
-        }
+        },
     },
 });
 
 const inpuut = document.querySelector("#phone");
 window.intlTelInput(inpuut, {
     initialCountry: "auto",
-    geoIpLookup: callback => {
+    geoIpLookup: (callback) => {
         fetch("https://ipapi.co/json")
-            .then(res => res.json())
-            .then(data => callback(data.country_code))
+            .then((res) => res.json())
+            .then((data) => callback(data.country_code))
             .catch(() => callback("ru"));
     },
-    utilsScript: "/intl-tel-input/js/utils.js?1695806485509" // just for formatting/placeholders etc
+    utilsScript: "/intl-tel-input/js/utils.js?1695806485509",
 });
